4b15e46728293ad8059479ff3a23fe2003e4c26b
trulybright <trulybright@yonsei.ac.kr>
2020-12-07 17:51:48 +0900

    SQL injection 취약점 방어

--- auth.py
+++ auth.py
@@ -2,15 +2,20 @@ import aiosqlite
 import sqlalchemy as sa
 from passlib.hash import sha256_crypt
 
-import db
+import re
 
+def proper_username(username):
+  return username.isalnum()
+
+def proper_nickname(nickname):
+  # 한글과 영어와 숫자로만 되었는지 판별하는 함수
+  return len(re.findall('[가-힣]|[a-z]|[A-Z]|[0-9]', username)) == len(username)
 
 
 async def check_credentials(username, password) -> bool:
   async with aiosqlite.connect('sql/users.db') as DB:
-    # where = sa.and_(db.users.c.username==username,
-    #             sa.not_(db.users.c.disabled))
-    # query = db.users.select().where(where)
+    if not proper_username(username):
+      return False
     query = f"SELECT * FROM users WHERE username='{username}'"
     cursor = await DB.execute(query)
     user = await cursor.fetchone()
@@ -22,15 +27,16 @@ async def check_credentials(username, password) -> bool:
 
 async def create_user(username, password, nickname) -> str:
   async with aiosqlite.connect('sql/users.db') as DB:
-    # where = sa.and_(db.users.c.username==username,
-    #                 sa.not_(db.users.c.disabled))
-    # query = db.users.select().where(where)
+    if not proper_username(username):
+      return 'improper username'
     query = f"SELECT * FROM users WHERE username='{username}'"
     cursor = await DB.execute(query)
     user = await cursor.fetchone()
     if user is not None: # username already occupied
       return 'username_duplicate'
 
+    if not proper_nickname(nickname):
+      return 'improper nickname'
     query = f"SELECT * FROM users WHERE nickname='{nickname}'"
     cursor = await DB.execute(query)
     user = await cursor.fetchone()
@@ -44,6 +50,7 @@ async def create_user(username, password, nickname) -> str:
 
 async def get_nickname(username) -> str:
   async with aiosqlite.connect('sql/users.db') as DB:
+    assert proper_username(username)
     query = f"SELECT nickname FROM users WHERE username='{username}'"
     cursor = await DB.execute(query)
     nickname = (await cursor.fetchone())[0]
