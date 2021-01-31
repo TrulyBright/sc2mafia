4c04aeb7ca78915dee84c5ddc8eda1eba2554b19
TrulyBright <trulybright@yonsei.ac.kr>
2021-02-01 01:00:04 +0900

    logger 추가

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import aioredis
 import aiohttp
+import logging
+from logging.handlers import RotatingFileHandler
 from sanic import Sanic
+from sanic.log import logger
 from jinja2_sanic import setup as setup_jinja2
 from jinja2 import FileSystemLoader
 from sanic_session import Session, AIORedisSessionInterface
@@ -10,12 +13,20 @@ from sio import setup_socketio
 
 app = Sanic("sc2mafia")
 
-# sanic_session initialization
+# sanic_session and logger initialization
 @app.listener("before_server_start")
-async def session_init(app, loop):
+async def session_and_logger_init(app, loop):
+    # session
     app.redis = await aioredis.create_redis_pool("redis://localhost")
     Session().init_app(app, interface=AIORedisSessionInterface(app.redis))
     app.oauth_client = aiohttp.ClientSession()
+    # logger
+    handler = RotatingFileHandler("sc2mafia.log", maxBytes=1024*16, backupCount=10)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter(fmt="%(asctime)s - (%(name)s)[%(levelname)s]: %(message)s",
+                                  datefmt="%Y-%m-%d %H:%M:%S")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 @app.listener('after_server_stop')
 async def client_close(app, loop):
@@ -26,3 +37,6 @@ app.static("/static", "./static")
 setup_routes(app)
 setup_jinja2(app, loader=FileSystemLoader("templates/"))
 setup_socketio(app)
+
+if __name__ == '__main__':
+    app.run(port=8000)
